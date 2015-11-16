380c1250eac78dc2b5b7acfa1af9ab4699544c72
Garp07 <gtliu@CSELAB5-013.cs.unl.edu>
2015-11-16 14:05:03 -0600

    Input login creds for mySQL

--- src/com/airamerica/jdbc/DatabaseInfo.java
+++ src/com/airamerica/jdbc/DatabaseInfo.java
@@ -10,9 +10,9 @@ public class DatabaseInfo {
 	 * I'll be removing this file from updating with future commits. 
 	 * Adjust to your login for testing.
 	 */
-	public static final String url = "jdbc:mysql://cse.unl.edu/YOURLOGIN";
-	public static final String username = "";
-	public static final String password = "YOURSQLPASSWORD";
+	public static final String url = "jdbc:mysql://cse.unl.edu/gtliu";
+	public static final String username = "gtliu";
+	public static final String password = "8vqbDP";
 	
 	static public Connection getConnection()
 	{
